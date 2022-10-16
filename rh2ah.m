683712c454c0c045798353fafa2074f2b9fa4224
Jacob Hall <jachallbound@gmail.com>
2022-10-16 14:58:49 -0400

    initial commit

---
+++ rh2ah.m
@@ -0,0 +1,10 @@
+%%% rh2ah.m
+% Function to convert relative humidity (rh) to absolute humidity (ah)
+% rh = relative humidity in percent (range: 0-1)
+% t_c = temperature in celsius
+% ah = absolute humidity in g/m^3
+
+function ah = rh2ah(rh, t_c)
+    assert(rh >= 0 && rh <= 1, "rh2ash() failed: rh must be between 0 and 1");
+    ah = (2.1667*100*rh*6.12*(10^((7.59*t_c)/(t_c+240.73))))/(t_c+273);
+end
