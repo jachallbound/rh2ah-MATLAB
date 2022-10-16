d62fb725efd0648a7bcd56bda34edbb94001d64d
Jacob Hall <jachallbound@gmail.com>
2022-10-16 15:14:20 -0400

    input can now be vectors

--- rh2ah.m
+++ rh2ah.m
@@ -5,6 +5,6 @@
 % ah = absolute humidity in g/m^3
 
 function ah = rh2ah(rh, t_c)
-    assert(rh >= 0 && rh <= 1, "rh2ash() failed: rh must be between 0 and 1");
-    ah = (2.1667*100*rh*6.12*(10^((7.59*t_c)/(t_c+240.73))))/(t_c+273);
+    assert(all(rh >= 0 & rh <= 1), "rh2ash() failed: rh must be between 0 and 1");
+    ah = (2.1667*100.*rh*6.12*(10.^((7.59.*t_c)/(t_c+240.73))))./(t_c+273);
 end
